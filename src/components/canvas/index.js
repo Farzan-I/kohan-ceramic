0649e80f4a5544fbd5b138bee1fe060838fe6b9a
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-22 22:17:12 +0000

    Add twin vase import/export

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,3 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
+import TwinVaseCanvas from "./Vases";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { TwinVaseCanvas };
