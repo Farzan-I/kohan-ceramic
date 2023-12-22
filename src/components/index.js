5d48c292501c4ce9881df98119f53388dac1dfdb
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-22 22:18:17 +0000

    Remove unused canvas imports, add twin vase

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { TwinVaseCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
@@ -13,8 +13,5 @@ export {
   Experience,
   Collection,
   Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
+  TwinVaseCanvas
 }
