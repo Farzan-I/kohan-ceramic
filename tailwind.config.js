cec363ce17db73aff1c2d3a4e73ffea20a053057
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-22 21:55:35 +0000

    change background image quality

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/sglbg.jpg')",
+        "hero-pattern": "url('/src/assets/sglbg.png')",
       },
     },
   },
